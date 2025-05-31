c11ce1a97a9364c7cc0ea019841e01308e4f2e4f
Anthony Suarez <asuarezop@gmail.com>
2025-05-31 10:41:48 -0400

    Removed globals.css from src/app and moved into styles/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="font-inter text-4xl underline">Test</h1>;
+  return <h1 className="font-inter text-yellow-55 text-4xl underline">Test</h1>;
 }
