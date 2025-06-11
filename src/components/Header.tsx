598049b8f3c3f8c93c5980bb18d6b2d0b5b7ea68
Anthony Suarez <asuarezop@gmail.com>
2025-06-11 00:11:30 -0400

    Adjusted styling for Header components across laptop and desktop breakpoints

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,14 @@ import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
 export default function Header() {
   return (
     <header>
-      <div className="bg-dark-08 flex justify-center-safe gap-2.5 px-6 pt-10 pb-3.5">
-        <h1 className="font-inter text-gray-60 w-[312px] text-[12px]">
+      <div className="bg-dark-08 3xl:py-4 flex justify-center gap-2.5 px-6 pt-10 pb-3.5 xl:gap-0 xl:px-6 xl:py-3">
+        <h1 className="font-inter text-gray-60 3xl:text-lg 3xl:w-[458px] w-[312px] text-[12px]">
           Subscribe to our Newsletter For Blogs and Resources
         </h1>
-        <ArrowUpRightIcon className="fill-yellow-55 h-5 w-5" />
+        <ArrowUpRightIcon className="fill-yellow-55 3xl:w-6 3xl:h-8 h-5 w-5" />
       </div>
 
-      <div className="bg-dark-10 flex justify-between border-y-1 px-4 py-5">
+      <div className="bg-dark-10 flex justify-between border-y-1 px-4 py-5 xl:px-20 xl:py-5">
         <Image src={FutureTechLogo} alt="FutureTech Blog logo" />
         <button className="bg-yellow-55 rounded-[6px] px-2.5 py-2.5 text-[14px]">
           Contact Us
