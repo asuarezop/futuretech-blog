6b1f311d3efd2b1236a428be3967ca90c249b636
Anthony Suarez <asuarezop@gmail.com>
2025-07-03 11:56:40 -0400

    Changed background color for container, added styling to text for main heading and supporting paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <h1 className="font-inter text-yellow-55 text-4xl underline">Test</h1>;
+  return (
+    <div className="bg-dark-08 px-4 py-6">
+      <h1 className="font-kumbh pb-3.5 text-[28px] font-medium text-white">
+        Today&apos;s Headlines: Stay Informed
+      </h1>
+      <p className="text-gray-60 font-inter text-[14px] leading-6 font-normal">
+        Explore the latest news from around the world. We bring you
+        up-to-the-minute updates on the most significant events, trends, and
+        stories. Discover the world through our news coverage.
+      </p>
+    </div>
+  );
 }
