d8a2357e8612c8c1d6191c2f92c61ad9be5aefa3
Anthony Suarez <asuarezop@gmail.com>
2025-07-06 22:50:03 -0400

    Added in category, date, and author for current Article in Headlines section

--- src/components/headlines/HeadlinePostCard.tsx
+++ src/components/headlines/HeadlinePostCard.tsx
@@ -10,14 +10,28 @@ export default function HeadlinePostCard(props: Article) {
           alt="post-img"
           width={390}
           height={213}
-          className="mb-[30px] rounded-xl"
+          className="mb-[1.875rem] rounded-xl"
         />
       </div>
-      <div>
+      <div className="mb-5">
         <h2 className="mb-2 text-xl font-semibold text-white">{props.title}</h2>
         <p className="text-gray-60 text-base leading-6 font-normal tracking-tight">
           {props.description}
         </p>
+      </div>
+      <div className="width-[320px] flex justify-between text-[14px]">
+        <div>
+          <h3 className="text-gray-60">Category</h3>
+          <p className="text-white">{props.category}</p>
+        </div>
+        <div>
+          <h3 className="text-gray-60">Publication Date</h3>
+          <p className="text-white">{props.date}</p>
+        </div>
+        <div>
+          <h3 className="text-gray-60">Author</h3>
+          <p className="text-white">{props.author}</p>
+        </div>
       </div>
     </section>
   );
