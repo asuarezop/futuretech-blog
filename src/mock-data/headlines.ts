a7db1526c919b0f4b64b6a989e1f7ec0fa2e8a85
Anthony Suarez <asuarezop@gmail.com>
2025-07-07 17:18:58 -0400

    Added new properties to Article type for likes and shares

---
+++ src/mock-data/headlines.ts
@@ -0,0 +1,15 @@
+import { Article } from "@/types/article";
+
+export const headlines: Article[] = [
+  {
+    image: "/images/post-1.png",
+    title: "Global Climate Summit Addresses Urgent Climate Action",
+    description:
+      "World leaders gathered at the Global Climate Summit to discuss urgent climate action, emissions reductions.",
+    category: "Environment",
+    date: "October 10, 2023",
+    author: "Jane Smith",
+    likes: "14K",
+    shares: "204",
+  },
+];
