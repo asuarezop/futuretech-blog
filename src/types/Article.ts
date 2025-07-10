dacc318a0c4d564fe71fa1f257b383b91c8558c4
Anthony Suarez <asuarezop@gmail.com>
2025-07-10 11:26:22 -0400

    Added new isFeatured property for Article type

--- src/types/Article.ts
+++ src/types/Article.ts
@@ -1,11 +1,12 @@
 export type Article = {
   image: string;
   title: string;
-  description: string;
-  category: string;
-  date: string;
-  author: string;
+  description?: string;
+  category?: string;
+  date?: string;
+  author?: string;
   likes?: string;
   shares?: string;
   slug: string;
+  isFeatured: boolean;
 };
