a319d6a105f304375a29df4e17234c415e45e564
Anthony Suarez <asuarezop@gmail.com>
2025-07-18 16:49:35 -0400

    Removed period from featured Article description

--- src/mock-data/headlines.ts
+++ src/mock-data/headlines.ts
@@ -5,7 +5,7 @@ export const headlines: Article[] = [
     image: "/images/post-1.png",
     title: "Global Climate Summit Addresses Urgent Climate Action",
     description:
-      "World leaders gathered at the Global Climate Summit to discuss urgent climate action, emissions reductions.",
+      "World leaders gathered at the Global Climate Summit to discuss urgent climate action, emissions reductions",
     category: "Environment",
     date: "October 10, 2023",
     author: "Jane Smith",
