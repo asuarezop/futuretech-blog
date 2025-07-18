000a1de9ef2ac0929d374eb7bab5bc444f2f0b1d
Anthony Suarez <asuarezop@gmail.com>
2025-07-17 23:51:19 -0400

    Changed Image component props alongside parent container to be more responsive, adjusted spacing between elements on desktop view

--- src/components/headlines/HeadlinePostCard.tsx
+++ src/components/headlines/HeadlinePostCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Article } from "@/types/article";
+import { Article } from "@/types/Article";
 import { HeartIcon } from "@heroicons/react/16/solid";
 import { ShareIcon } from "@heroicons/react/16/solid";
 import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
@@ -8,30 +8,41 @@ import Link from "next/link";
 export default async function HeadlinePostCard(props: Article) {
   return (
     <>
-      <div className="">
-        <div className="relative pt-10 xl:pt-0">
+      <div className={`xl:py-0 ${props.index === 2 ? "py-[30px]" : ""}`}>
+        <div className="relative mb-4 h-[185px] w-[358px] xl:mb-5 xl:w-[407px]">
           <Image
             src={props.image}
             alt="post-img"
-            width={390}
-            height={185}
-            className="mb-5 rounded-[10px]"
+            fill
+            style={{ objectFit: "cover" }}
+            className="rounded-[10px]"
           />
         </div>
-        <div>
+
+        {/* <div className="relative pt-10 sm:hidden xl:mb-5 xl:h-[185px] xl:w-[390px] xl:pt-0">
+          <Image
+            src={props.image}
+            alt="post-img"
+            fill
+            objectFit="cover"
+            className="rounded-[10px]"
+          />
+        </div> */}
+
+        <div className="w-[358px] xl:w-[407px]">
           <h2 className="mb-1 text-[16px] font-semibold text-white">
             {props.title}
           </h2>
         </div>
-        <div className="mb-3 flex w-[320px] justify-between text-[14px]">
+        <div className="mb-3 w-[320px] text-[14px]">
           <div>
             <p className="text-gray-60 text-[14px] font-normal">
               {props.category}
             </p>
           </div>
         </div>
-        <div className="flex justify-between gap-4 xl:w-[390px]">
-          <div className="flex w-[155px] items-center">
+        <div className="flex justify-between gap-4 xl:w-[407px]">
+          <div className="flex w-[155px] items-center xl:w-[207px]">
             <button className="text-gray-60 hover:bg-dark-20 bg-dark-10 font-kumbh outline-dark-15 mr-2 flex rounded-[100px] px-3.5 py-1.5 text-[14px] font-normal outline-1 hover:cursor-pointer">
               <HeartIcon className="mr-1 h-5 w-5" />
               {props.likes}
@@ -41,8 +52,8 @@ export default async function HeadlinePostCard(props: Article) {
               {props.shares}
             </button>
           </div>
-          <Link href={`/posts/${props.slug}`}>
-            <button className="bg-dark-08 text-gray-60 outline-dark-15 font-inter hover:bg-dark-20 flex w-[135px] justify-between rounded-[8px] px-5 py-3.5 text-[14px] font-normal outline-1 hover:cursor-pointer">
+          <Link className="" href={`/posts/${props.slug}`}>
+            <button className="bg-dark-08 text-gray-60 outline-dark-15 font-inter hover:bg-dark-20 flex w-[135px] justify-between rounded-[8px] px-5 py-3.5 text-[14px] font-normal outline-1 hover:cursor-pointer xl:w-[201px] xl:px-13">
               Read More
               <ArrowUpRightIcon className="fill-yellow-55 h-5 w-5" />
             </button>
